8ef0db67f48b8d3003d5bf63e1e11416179e8f90
waritk <84290064+waritk@users.noreply.github.com>
2025-02-20 04:26:17 +0700

    create activities row

---
+++ BrainHealth/Component/ActivityRow.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityRow.swift
+//  BrainHealth
+//
+//  Created by Warit Karnbunjob on 20/2/2568 BE.
+//
+
+import SwiftUI
+
+struct ActivityRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityRow()
+}
---
+++ BrainHealth/Component/StatusCard.swift
@@ -0,0 +1,24 @@
+struct StatusCard: View {
+    let title: String
+    let count: Int
+    let color: Color
+    
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.gray)
+            Text("\(count)")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(color)
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 12)
+                .fill(Color.white)
+                .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
+        )
+    }
+}
